71226664792d0b9402b287ae47ce46b5b06f9114
Wilbert <40409363+LaTola@users.noreply.github.com>
2023-09-25 20:02:41 -0300

    Vote method implementation

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -12,6 +12,25 @@ router = APIRouter(prefix="/vote", tags=['vote'])
 
 @router.post("/", status_code=status.HTTP_200_OK)
 async def vote(vote: schemas.Vote, db: Session = Depends(get_db), user=Depends(oauth2.get_current_user)):
+    """
+    Vote post functionality
+
+    Args:
+        vote (schemas.Vote): The vote 
+        db (Session, optional): db pgsql session. Defaults to Depends(get_db).
+        user (_type_, optional): the vote user (get by request or from session in key). Defaults to Depends(oauth2.get_current_user).
+
+    Raises:
+        HTTPException: 404 if post to be meant to be voted does not exists
+
+    Returns:
+        str: a message confirming if vote or unvote have been registered on database.
+
+    Comments: First time a user call vote on a post, the record and positive vote is recorded.
+    The second time the same user vote the same post, the existing vote will be deleted from db as on any social media like works.
+    This functionality does not work exactly as on course, originally it sends an extra parameter on the request specifying the direction
+    (1 or 0) to add or delete a vote but personally I think it get the method more complicated unnecessarily.
+    """
 
     # Check if post exists
     post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
