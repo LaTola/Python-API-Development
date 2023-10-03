5d46a56b20c6850c8535af06f2b2bd6cf058876b
Wilbert <40409363+LaTola@users.noreply.github.com>
2023-10-03 10:54:24 -0300

    update entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-cd /app
-alembic upgrade head
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+#!/bin/bash 
+alembic upgrade head 
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
